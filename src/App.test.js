5621a8d39edebb3ae81ea76d2757cf987ee005e1
Yongun Seong <nevivurn@riseup.net>
2019-10-07 17:28:08 +0900

    test App.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { shallow, mount } from 'enzyme';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+
+
+test('Root Component', () => {
+  const component = shallow(<App/>);
+  expect(component.find('.App').length).toBe(1);
 });
