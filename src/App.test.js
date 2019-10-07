5525068a5fabf4f9f65d1928252945b1650b180c
Yongun Seong <nevivurn@riseup.net>
2019-10-07 17:36:40 +0900

    clean up App.js tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import { shallow, mount } from 'enzyme';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import reducer from './reducers';
-
+import { shallow } from 'enzyme';
 
 test('Root Component', () => {
   const component = shallow(<App/>);
